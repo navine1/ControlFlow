6c517456ec52e4d8f3cdaf554ad149024d3d2588
navine1 <thanavine.lecocq@eduvaud.ch>
2024-09-27 13:59:04 +0200

    1.0

--- src/weeklyDayLoop.js
+++ src/weeklyDayLoop.js
@@ -1,10 +1,10 @@
 function getDayLoop(day) {
     const jours = ["lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi", "dimanche"];
 
-    // regarde si le numero est juste
-    if (day < 1 || day > 7) {
-        return "erreur";
-    }
+//    // regarde si le numero est juste
+//    if (day < 1 || day > 7) {
+//        return "erreur";
+//    }
 
     // Boucle pour retrouver le jour
     for (let i = 0; i < jours.length; i++) {
@@ -12,6 +12,8 @@ function getDayLoop(day) {
             return jours[i];
         }
     }
+    return "erreur";
 }
+
 
 console.log(getDayLoop(3));  // "mercredi"
